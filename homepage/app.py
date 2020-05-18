900bc48936f8de6f9fbb788f879ffc82a3ab8a42
quockhanh21299 <37074707+quockhanh21299@users.noreply.github.com>
2020-05-19 00:10:26 +0700

    update

--- homepage/app.py
+++ homepage/app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('new_cnn.html')
+    return render_template('CNN2.html')
 
 @app.route('/classify', methods=['POST'])
 def predict():
-    model = tf.keras.models.load_model('../trained-model.h5', compile=False)
+    model = tf.keras.models.load_model('../trained-model_new.h5', compile=False)
 
     try:
         #Processing input
@@ -26,17 +26,17 @@ def predict():
         img = request.files['imageFile']
         img.save('input.jpg')
         img_array = cv2.imread("input.jpg")
-
+        img_array=img_array/255
         new_array = cv2.resize(img_array, (IMG_SIZE,IMG_SIZE))
         image = new_array.reshape(1, IMG_SIZE, IMG_SIZE, 3)
-
+        
         predict = model.predict(image)
         predict_class = CATEGORIES[np.argmax(predict)]
 
         if (predict_class == 'Human'):
-            message = 'Congrats, you are one of us! \n Click somewhere to proceed...'
+           message = 'Congrats, you are one of us! \n Click somewhere to proceed...'
         else:
-            message = 'Ehh you are one of the ' + predict_class + '. ' + predict_class + ' are not allowed here...' 
+           message = 'Nooo, you upload a photo of  ' + predict_class + '.I thought you are human,not ' + predict_class + ', Please use real image :)' 
 
         print(predict_class)
 
